399ad8e254bebcb52dc6732d4cd7f948b7e727b4
Hase401 <ponta410422@icloud.com>
2021-07-14 21:04:10 +0900

    StoryboardでStackViewの練習をしたので追加

---
+++ BasicAutoLayout_Practice1/BasicAutoLayout_Practice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicAutoLayout_Practice1
+//
+//  Created by 長谷川孝太 on 2021/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
