53bf3a6ca2830a5c0fbd70481e8d4a9e83be0004
Hase401 <ponta410422@icloud.com>
2021-07-19 23:57:57 +0900

    止まるViewを作るときの下準備を追加

---
+++ ScrollViewAutoLayout_Practice2/ScrollViewAutoLayout_Practice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewAutoLayout_Practice2
+//
+//  Created by 長谷川孝太 on 2021/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
