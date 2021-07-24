77df75f2a45313abc5c3f31f576b6fd2893d0391
Hase401 <ponta410422@icloud.com>
2021-07-24 23:20:53 +0900

    xibを使ってScrollするtabButtonを実装して追加

--- ScrollViewAutoLayout_Practice3/ScrollViewAutoLayout_Practice3/FirstSwitcherView/FirstSwitcherView.swift
+++ ScrollViewAutoLayout_Practice3/ScrollViewAutoLayout_Practice3/FirstSwitcherView/FirstSwitcherView.swift
@@ -1,34 +1,29 @@
 //
-//  UserInfoView.swift
-//  BasicAutoLayout_Practice2
+//  FirstSwitcherView.swift
+//  ScrollViewAutoLayout_Practice3
 //
-//  Created by 長谷川孝太 on 2021/07/17.
+//  Created by 長谷川孝太 on 2021/07/24.
 //
 
 import UIKit
 
-class FirstView: UIView {
+public class FirstSwitcherView: UIView {
 
-    @IBOutlet private weak var sampleButton: UIButton!
-    @IBOutlet private weak var sampleLabel: UILabel!
-
-    // 【疑問】セットで必要？
     override init(frame: CGRect) {
         super.init(frame: frame)
-
         nibInit()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-
         nibInit()
     }
 
     private func nibInit() {
-        let nib = UINib(nibName: "FirstView", bundle: nil)
+        let nib = UINib(nibName: "FirstSwitcherView", bundle: nil)
         guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else { return }
         view.frame = self.bounds
         self.addSubview(view)
     }
+
 }
---
+++ ScrollViewAutoLayout_Practice3/ScrollViewAutoLayout_Practice3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewAutoLayout_Practice3
+//
+//  Created by 長谷川孝太 on 2021/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
