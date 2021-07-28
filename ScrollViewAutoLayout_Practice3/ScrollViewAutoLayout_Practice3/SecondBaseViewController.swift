1ccffab7dc01b3a13d8f9defb1be95824ca6709c
Hase401 <ponta410422@icloud.com>
2021-07-28 11:16:29 +0900

    基本のTabの実装が完成したので追加

--- ScrollViewAutoLayout_Practice3/ScrollViewAutoLayout_Practice3/SecondBaseViewController.swift
+++ ScrollViewAutoLayout_Practice3/ScrollViewAutoLayout_Practice3/SecondBaseViewController.swift
@@ -10,37 +10,47 @@ import UIKit
 class SecondBaseViewController: SecondDefaultViewController {
 
 /// 【疑問】Baseにはまだ書かない？
-//    override var titlesInSwitcher: [String] {
-//        ["Swift", "Ruby", "Realm", "Firebase"]
-//    }
+    override var titlesInSwitcher: [String] {
+        ["Swift", "Ruby", "Realm", "Firebase"]
+    }
 
     override var switcherConfig: SwitcherConfig {
-        return ConfigManager.shared.switcherConfig
+        var config = ConfigManager.shared.switcherConfig
+        config.type = .tab　// 横いっぱいに広がる
+//        config.type = .segement // 横いっぱいに広がらない
+        return config
     }
+    //    override var switcherConfig: SwitcherConfig {
+    //        return ConfigManager.shared.switcherConfig
+    //    }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
-    }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
-    }
+    // 削除⭕
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
+//    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
+//    }
+//
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//        debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
+//    }
 
     /// 【重要】navigationItemの設定
     override func viewDidLoad() {
         super.viewDidLoad()
+        defaultSelectedIndex = 0
+        reloadData()
         debugPrint("\(type(of: self)) - \(String(format: "%p", self)) - \(#function)")
     }
 
